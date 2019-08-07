e66a3d0c9481e8569737d9ed555088583e2ffd7b
tnmarsha <tnmarsha@geneva.edu>
2019-08-07 00:40:18 -0400

    update

---
+++ app/Http/Controllers/ApiMeetingCotroller.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Meeting;
+class ApiMeetingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Meeting::get();
+    }
+    
+    /**
+     * Return attendees for a particular customer
+     * @param $id
+     * @return int
+     */
+    public function attendees($id)
+    {
+        $attendees = Meeting::where('id', $id)
+            ->with('attendees')
+            ->get();
+        
+        return $attendees;
+    }
+    
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Meeting::find($id);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
