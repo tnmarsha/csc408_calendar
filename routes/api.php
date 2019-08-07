1168b9795107d25bbe1e72fbf9e4882b038192fc
tnmarsha <tnmarsha@geneva.edu>
2019-08-07 00:41:25 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,15 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/users/{id}/attendees','ApiUserController@Attendees');
+Route::resource('/users','ApiUserController');
+
+Route::get('/meetings/{id}/attendees','ApiMeetingController@Attendees');
+Route::resource('/meetings','ApiMeetingController');
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//////////////////////////////
